d4d40550b2eb44763c7303d92218f1e5b228afbf
Kiki <kabdollahi347@gmail.com>
2023-03-22 12:49:00 -0700

    Area of circle exercise

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-function greet(name, time= "day") {
-    console.log(`Good ${time} ${name}!`);
+// function greet(name, time= "day") {
+//     console.log(`Good ${time} ${name}!`);
 
-}
+// }
 
-greet("Kian", "hour");
+// greet("Kian", "morning");
 
 
 // let greet = (name) => {
@@ -12,4 +12,15 @@ greet("Kian", "hour");
 
 // greet("Kian")
 
+function circleArea(radius) {
+    let area = (Math.PI)*((radius)**2)
+    alert(`The area of the circle with radius ${radius} is ${area}`)
+}
+let input;
+do{
+input = prompt("Please enter a radius: ");
+}
+while (isNaN(input) == true);
+
+circleArea(input)
 
