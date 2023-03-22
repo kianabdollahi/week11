a73ea40be011ed30695f209dd448f87031b0dbde
Kiki <kabdollahi347@gmail.com>
2023-03-22 13:43:29 -0700

    Text replacment exercise

--- scripts/script.js
+++ scripts/script.js
@@ -5,22 +5,39 @@
 
 // greet("Kian", "morning");
 
-
 // let greet = (name) => {
 //     console.log(`Hello ${name}!`);
 // }
 
 // greet("Kian")
 
+// function circleArea(radius) {
+//   let area = Math.PI * radius ** 2;
+//   alert(`The area of the circle with radius ${radius} is ${area}`);
+// }
+// let input;
+// do {
+//   input = prompt("Please enter a radius: ");
+// } while (isNaN(input) == true);
+
+// circleArea(input);
+
+const radiusPara = document.querySelector("#radius");
+console.log(radiusPara);
+const resultPara = document.querySelector("#result");
+console.log(resultPara);
+
 function circleArea(radius) {
-    let area = (Math.PI)*((radius)**2)
-    alert(`The area of the circle with radius ${radius} is ${area}`)
+  let area = Math.PI * radius ** 2;
+  alert(`The area of the circle with radius ${radius} is ${area}`);
+  resultPara.textContent = `The area of the circle with radius ${radius} is ${area}`;
 }
+
 let input;
-do{
-input = prompt("Please enter a radius: ");
-}
-while (isNaN(input) == true);
+do {
+  input = prompt("Please enter a radius: ");
+} while (isNaN(input) == true);
 
-circleArea(input)
+radiusPara.textContent += `The radius you entered was ${input}`;
 
+circleArea(input);
